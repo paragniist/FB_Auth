418f6edfc801644317646d325ca95f178c87e855
Parag <paragniist@gmail.com>
2018-04-13 15:58:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/maheshthippala/fb_auth/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.maheshthippala.fb_auth;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1,et2;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void login(View view) {
+    }
+
+    public void register(View view) {
+
+        mAuth.createUserWithEmailAndPassword(et1.getText().toString(),
+                et2.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+}
---
+++ app/src/main/java/com/example/maheshthippala/fb_auth/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.maheshthippala.fb_auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void logout(View view) {
+    }
+
+
+}
